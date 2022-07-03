e1157e90347f344b6a908132f6bab053ebdbddc7
Rodrigo Dias <rodrigo.elfo@gmail.com>
2022-07-03 20:55:26 -0300

    add - screen - home

---
+++ src/app/screens/Home/Home.tsx
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+
+const Home = () => {
+  return (
+    <S.HomeContainer>
+      <div>bla</div>
+      <div>bla</div>
+      <div>bla</div>
+      <div>bla</div>
+    </S.HomeContainer>
+  );
+};
+
+export default Home;
---
+++ src/app/screens/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  max-width: 1320px;
+  display: flex;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
+`;
